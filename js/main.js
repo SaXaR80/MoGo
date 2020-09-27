c88e58f3fb8795da17160a4877210bb05f3e8149
saxar <ostinbobik@gmail.com>
2020-09-27 20:04:13 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.png" alt=""></button>'
+  });
+});
