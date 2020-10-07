4db136dd50cda8b675c76bfd30fb2fa255c868cb
saxar <ostinbobik@gmail.com>
2020-10-07 13:57:09 +0300

    pravki

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(function(){
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.png" alt=""></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.png" alt=""></button>'
   });
+
+  $('.menu__mobil').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
 });
